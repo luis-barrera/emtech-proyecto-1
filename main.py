72c7929ffce46847c46eb00b1d6a9cdac9336c4b
Luis Barrera <luisvaldovinosbarrera@gmail.com>
2022-01-30 13:05:42 -0600

    Termina parte de productos más comprados y más buscados

--- main.py
+++ main.py
@@ -57,6 +57,8 @@ while usuario_autenticado:
 #####################################
 # Diccionario que lleva la cuenta de las ventas por cada producto
 contadores_ventas = {}
+# Lista con los productos más vendidos
+prod_mas_vendidos = []
 
 # Recorremos toda la lista de ventas
 for venta in lifestore_sales:
@@ -65,13 +67,76 @@ for venta in lifestore_sales:
     else:
         contadores_ventas[venta[1]] = 1
 
-print(contadores_ventas)
+# Ordenamos los elementos de mayor a menor
+ventas_ordenadas = dict(sorted(contadores_ventas.items(),
+                               key = lambda item: item[1],
+                               reverse = True))
+
+# Agregamos a cada venta el nombre del producto y mostramos los productos
+contador = 0
+
+print("Mostrando productos más vendidos")
+# Recorremos toda la lista
+for contador_ventas in ventas_ordenadas.items():
+    for producto in lifestore_products:
+        if producto[0] == contador_ventas[0]:
+            prod_mas_vendidos.append([*contador_ventas, producto[1]])
+            break
+
+    if contador == 5: break
+
+    contador += 1
 
 
+# TODO: mostrar los productos de mejor manera
+print(prod_mas_vendidos, "\n\n")
+
+
+# Diccionario que lleva la cuenta de las ventas por cada producto
+contadores_busquedas = {}
+# Lista con los productos más vendidos
+prod_mas_buscados = []
+
+# Recorremos toda la lista de ventas
+for busqueda in lifestore_searches:
+    if busqueda[1] in contadores_busquedas:
+        contadores_busquedas[busqueda[1]] += 1
+    else:
+        contadores_busquedas[busqueda[1]] = 1
+
+# Ordenamos los elementos de mayor a menor
+busquedas_ordenadas = dict(sorted(contadores_busquedas.items(),
+                               key = lambda item: item[1],
+                               reverse = True))
+
+# Agregamos a cada busqueda el nombre del producto y mostramos los productos
+contador = 0
+print("Mostrando productos más buscados")
+# Recorremos toda la lista
+for contador_busquedas in busquedas_ordenadas.items():
+    for producto in lifestore_products:
+        if producto[0] == contador_busquedas[0]:
+            prod_mas_buscados.append([*contador_busquedas, producto[1]])
+            break
+
+    if contador == 10: break
+
+    contador += 1
+
+
+# TODO: mostrar los productos de mejor manera
+print(prod_mas_buscados)
+
+
+# print(ventas_ordenadas.items())
 ###################################
 ## Productos más buscados #########
 ###################################
+#lifestore_searches = [id_search, id product]
 # print(lifestore_searches[0])
+
+#lifestore_products = [id_product, name, price, category, stock]
 # print(lifestore_products[0])
+
 # lifestore_sales = [id_sale, id_product, score (from 1 to 5), date, refund (1 for true or 0 to false)]
 # print(lifestore_sales[0])
